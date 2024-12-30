751d2218e06c65e64da6213bb94571c1136e31b7
morningcoder24 <tranquockhanh9a9@gmail.com>
2024-12-30 16:33:50 +0700

    feat: cắt layout admin

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,37 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.admin');
 });
+
+Route::get('/admin', function () {
+    return view('admin.home.index');
+})->name('admin.home.index');
+
+Route::get('/admin/qldonhang', function () {
+    return view('admin.qldonhang.index');
+})->name('admin.qldonhang.index');
+
+Route::get('/admin/qlkhachhang', function () {
+    return view('admin.qlkhachhang.index');
+})->name('admin.qlkhachhang.index');
+
+Route::get('/admin/qlnhanvien', function () {
+    return view('admin.qlnhanvien.index');
+})->name('admin.qlnhanvien.index');
+
+Route::get('/admin/qlsanpham', function () {
+    return view('admin.qlsanpham.index');
+})->name('admin.qlsanpham.index');
+
+Route::get('/admin/qldanhmuc', function () {
+    return view('admin.qldanhmuc.index');
+})->name('admin.qldanhmuc.index');
+
+Route::get('/admin/qllienhe', function () {
+    return view('admin.qllienhe.index');
+})->name('admin.qllienhe.index');
+
+Route::get('/admin/qlbinhluan', function () {
+    return view('admin.qlbinhluan.index');
+})->name('admin.qlbinhluan.index');

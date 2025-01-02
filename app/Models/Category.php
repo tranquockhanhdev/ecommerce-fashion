39d9102c8e2a4f2c436282ff7e58996079065e6b
pkhang35 <pkhang353@gmail.com>
2025-01-02 18:56:50 +0700

    feat: Add model quan ly san pham refer: #9

---
+++ app/Models/Category.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+
+    protected $table = 'categorie';
+
+
+    protected $fillable = [
+        'parent_id',
+        'name',
+        'slug',
+        'image',
+        'status',
+    ];
+
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id'); // Một danh mục có thể có một danh mục cha
+    }
+
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id'); // Một danh mục có thể có nhiều danh mục con
+    }
+
+
+    public function products()
+    {
+        return $this->hasMany(Product::class); // Một danh mục có thể có nhiều sản phẩm
+    }
+}
